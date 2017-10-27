d7b0b88dc8fcc52e93d26d8fc19e5a394969472c
Unknown <flavyu96@gmail.com>
2017-10-27 17:23:18 +0300

    Edit clicked listener

--- app/src/main/java/com/gym/app/parts/profile/ProfileFragment.java
+++ app/src/main/java/com/gym/app/parts/profile/ProfileFragment.java
@@ -1,7 +1,15 @@
 package com.gym.app.parts.profile;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.Toast;
+
 import com.gym.app.R;
 import com.gym.app.parts.home.BaseHomeFragment;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * @author Paul
@@ -9,6 +17,19 @@ import com.gym.app.parts.home.BaseHomeFragment;
  */
 
 public class ProfileFragment extends BaseHomeFragment {
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        ButterKnife.bind(this, view);
+    }
+
+
+    @OnClick(R.id.profile_image)
+    void clickedImage(){
+        Toast.makeText(getContext(), "to be edit", Toast.LENGTH_SHORT).show();
+    }
+
     @Override
     protected int getLayoutId() {
         return R.layout.fragment_profile;
